fbd9e771bd8f68d69c161691f18100f870d71dfb
Ryuash <42913823+ryuash@users.noreply.github.com>
2022-04-18 22:58:05 -0400

    Update/css (#7)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -15,13 +15,13 @@ function MyApp({ Component, pageProps }) {
         <meta name="description" content="Porfolio Website of @ryuash" />
         <meta name="og:description" property="og:description" content="Porfolio Website of @ryuash" />
         <meta name="og:title" property="og:title" content="Ryuash" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-touch-icon.png" />
+        {/* <link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-touch-icon.png" /> */}
         <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png" />
-        <link rel="manifest" href="/icons/site.webmanifest" />
+        {/* <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png" /> */}
+        {/* <link rel="manifest" href="/icons/site.webmanifest" /> */}
         <meta name="twitter:card" content="summary" />
         <meta name="robots" content="noindex, nofollow" />
-        <meta property="og:image" content="https://i.imgur.com/Pn5SFLb.png" />
+        <meta property="og:image" content="/icons/favicon-32x32.png" />
         <title>Ryuash</title>
       </Head>
       <ThemeProvider theme={theme}>
--- src/screens/home/components/nav/components/desktop/index.tsx
+++ src/screens/home/components/nav/components/desktop/index.tsx
@@ -7,7 +7,12 @@ const Desktop = () => {
   return (
     <RootCSS>
       <div className="nav">
-        <p className="nav--title">Ryuash</p>
+        <AnchorLink
+          href='#about'
+          offset='60'
+        >
+          <h2 className="nav--title">Ryuash</h2>
+        </AnchorLink>
         <div className="menu">
           {navItems.map((x) => (
             <AnchorLink href={x.to} offset='60' key={x.to} className="menu--item">
