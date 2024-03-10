44e84379438225e8994d8b8f5b9a34fb5def02f4
Wingman <ryuash123@gmail.com>
2024-03-09 22:57:58 -0500

    .

--- scripts/deploy-github.js
+++ scripts/deploy-github.js
@@ -8,7 +8,10 @@ ghpages.publish(
     repo: 'https://github.com/ryuash/ryuash.github.io.git',
     history: false
   },
-  () => {
+  (err) => {
+    if (err) {
+      console.log(err.message)
+    }
     console.log('Deploy Complete!');
   },
 );
